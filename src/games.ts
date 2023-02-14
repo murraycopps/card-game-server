1bef9db2278656003e252f814e95519f9eecc30f
murraycopps <murraycopps@gmail.com>
2023-02-14 07:32:12 -0500

    first commit

---
+++ src/games.ts
@@ -0,0 +1,81 @@
+type Game = {
+    id: number;
+    started: boolean;
+    winner?: Player;
+    name: string;
+    players: Player[];
+}
+
+type Card = {
+    number: number;
+    suit: string;
+}
+
+type Player = {
+    id: number;
+    chosen: boolean;
+    name: string;
+    cards: Card[];
+    playedCard?: Card;
+}
+
+const games: Game[] = []
+
+const findMaxID = (): number => {
+    let maxID = 0;
+    games.forEach(game => {
+        if (game.id > maxID) {
+            maxID = game.id;
+        }
+    });
+    return maxID;
+}
+
+const createGame = (name: string): Game => {
+    const game: Game = {
+        id: findMaxID() + 1,
+        started: false,
+        name: name,
+        players: []
+    }
+    dealCards(game);
+    games.push(game);
+    return game;
+}
+
+const addPlayer = (game: Game, name: string): Player => {
+    const player: Player = {
+        id: game.players.length + 1,
+        chosen: false,
+        name: name,
+        cards: []
+    }
+    game.players.push(player);
+    return player;
+}
+
+
+const dealCards = (game: Game) => {
+    const cards: Card[] = [];
+    for (let i = 2; i <= 14; i++) {
+        cards.push({ number: i, suit: 'hearts' });
+        cards.push({ number: i, suit: 'diamonds' });
+        cards.push({ number: i, suit: 'clubs' });
+        cards.push({ number: i, suit: 'spades' });
+    }
+    // shuffle the cards
+    for (let i = cards.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * i);
+        const temp = cards[i];
+        cards[i] = cards[j];
+        cards[j] = temp;
+    }
+    // deal the cards
+    for (let i = 0; i < cards.length; i++) {
+        game.players[i % game.players.length].cards.push(cards[i]);
+    }
+}
+
+
+
+export { games, createGame, Game, Card, Player, addPlayer }
---
+++ src/index.ts
@@ -0,0 +1,142 @@
+import express from "express";
+import bodyParser from "body-parser";
+import { games, Game, Card, createGame, addPlayer } from "./games";
+
+const app = express();
+
+// create application/json parser
+const jsonParser = bodyParser.json()
+
+// create application/x-www-form-urlencoded parser
+const urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+})
+
+app.get('/games', (req, res) => {
+    res.send(games);
+})
+
+app.post('/games', jsonParser, (req, res) => {
+    const name = req.body.name;
+    if (!name) {
+        res.status(400).send('Name is required');
+        return;
+    }
+    const game = games.find(g => g.name === name);
+    if (game) {
+        res.status(400).send('Game already exists');
+        return;
+    }
+    const newGame = createGame(name);
+    res.send(newGame);
+})
+
+
+app.get('/games/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const game = games.find(g => g.id === id);
+    if (game) {
+        res.send(game);
+    } else {
+        res.status(404).send('Game not found');
+    }
+})
+
+// join the game as a player
+app.post('/games/:id/join', jsonParser, (req, res) => {
+    const id = parseInt(req.params.id);
+    const game = games.find(g => g.id === id);
+    if (game) {
+        const name = req.body.name;
+        if (!name) {
+            res.status(400).send('Name is required');
+            return;
+        }
+        const player = game.players.find(p => p.name === name);
+        if (player) {
+            res.status(400).send('Player already exists');
+            return;
+        }
+        const newPlayer = addPlayer(game, name);
+        res.send(newPlayer);
+    } else {
+        res.status(404).send('Game not found');
+    }
+})
+
+// if both players have played a card check which card is higher and give both cards to the winner
+// if only one player has played a card, do nothing
+// if neither player has played a card, do nothing
+const checkForWinner = (game: Game) => {
+    const player1 = game.players[0];
+    const player2 = game.players[1];
+    if (player1.playedCard && player2.playedCard) {
+
+        if (player1.playedCard.number > player2.playedCard.number) {
+            player1.cards.push(player1.playedCard);
+            player1.cards.push(player2.playedCard);
+        } else {
+            player2.cards.push(player1.playedCard);
+            player2.cards.push(player2.playedCard);
+        }
+        player1.playedCard = undefined;
+        player2.playedCard = undefined;
+        // check if either player has won
+        if (player1.cards.length === 0) {
+            game.winner = player1;
+        }
+        if (player2.cards.length === 0) {
+            game.winner = player2;
+        }
+    }
+}
+
+// get the card that the player wants to play from the request body
+// set the playedCard property on the player to the card they want to play
+// call the checkForWinner function
+// send the game back to the client
+app.post('/games/:id/play', jsonParser, (req, res) => {
+    const id = parseInt(req.params.id);
+    const game = games.find(g => g.id === id);
+    if (game) {
+        const card = req.body.card;
+        console.log(card);
+
+        if (!card) {
+            res.status(404).send('Card not found');
+            return;
+        }
+
+        // find player with the card
+        const player = game.players.find(p => p.cards.find(c => c.number === card.number && c.suit === card.suit));
+        if (!player) {
+            res.status(404).send('Player not found');
+            return;
+        }
+
+        // if player has played a card already do nothing
+        if (player.playedCard) {
+            console.log(game)
+            res.status(400).send('Player has already played a card');
+            return;
+        }
+        player.playedCard = card;
+        // remove card from player
+        player.cards = player.cards.filter(c => c.number !== card.number || c.suit !== card.suit);
+        checkForWinner(game);
+
+        res.send(game);
+
+
+
+    } else {
+        res.status(404).send('Game not found');
+    }
+})
+
+
+app.listen(5000, () => {
+    console.log('Server is running...');
+})
